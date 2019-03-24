2843ed9b20876dbfaaa52d08e40e2d6eaac57832
Samuel Bowman <samuelpbowman@gmail.com>
2019-03-23 21:07:48 -0400

    get candidates' ten recent tweets

---
+++ README.md
@@ -0,0 +1,3 @@
+# candidate-scanner
+
+A Python program to keep tabs on the Democratic candidates in the 2020 U.S. Presidential campaign.
--- src/main.py
+++ src/main.py
@@ -3,3 +3,19 @@ import json
 import config as config
 
 auth = tweepy.OAuthHandler(config.api_key,config.api_secret)
+auth.set_access_token(config.access_token,config.access_secret)
+
+twitter = tweepy.API(auth)
+
+candidates = []
+
+with open("src/resources/dem_candidates.json","r") as candidate_file:
+    candidate_data = json.load(candidate_file)
+    candidates = candidate_data["candidates"]
+
+profiles = [twitter.get_user(candidate["handle"])
+        for candidate in candidates]
+
+top_tens = [twitter.user_timeline(screen_name=candidate["handle"],count=10)
+        for candidate in candidates]
+
